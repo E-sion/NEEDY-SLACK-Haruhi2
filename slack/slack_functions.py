b04357b7b146a6205ecbc7bec8a97f58f38e5d4a
E-sion <101786678+E-sion@users.noreply.github.com>
2023-11-24 11:41:51 +0800

    转移回复方法

--- slack/slack_functions.py
+++ slack/slack_functions.py
@@ -1,10 +1,11 @@
 import os
+import re
 
 from slack.slack_utils import get_random_thinking_message, send_slack_message_and_return_message_id, divede_sentences, \
-    run
+    run, SlackStreamingCallbackHandler
 
 
-def slack_respond_with_agent(event, ack, app, prompt, say):
+def slack_respond_with_agent(event, ack, app, prompt):
     """
     This function takes a Slack message event and respond with a LLM-generated response
     chinese:  该函数接受Slack消息事件，并使用LLM生成的响应进行响应
@@ -18,31 +19,12 @@ def slack_respond_with_agent(event, ack, app, prompt, say):
         app=app, channel=channel, message=get_random_thinking_message())
 
     # Generate an LLM response using agent chinese:  使用代理生成LLM响应
-    user_query = event["text"]
-
+    if "@" in event["text"]:
+        user_query = re.sub("<@.*>", "", event["text"])
+    else:
+        user_query = event["text"]
     print("user: ", user_query)
-    # 调用函数
-    # response = try_keys(keys, user_query, prompt)
 
     user_name = os.environ["USER_NAME"]
-
-    # 似乎直接return的内容是一段完整的对话
-    response = run(user_name, user_query, prompt)
-
-    # todo 到底要不要添加这个功能呢？
-    # 把AI的回复内容重新处理成多端对话之后再发送
-
-    # # 调用分割bot回复的函数
-    # sentences = divede_sentences(response)
-    # # 将所有回复拼接成一个字符串
-    # combined_response = '\n'.join(sentences)
-
-    # Replace acknowledgement message with actual response chinese: 循环用实际响应替换确认消息
-    ack()
-    say(response)
-
-    # app.client.chat_update(
-    #     channel=channel,
-    #     text=response,
-    #     ts=ack_message_id,
-    # )
+    callback = SlackStreamingCallbackHandler(ts=ack_message_id, channel=channel)
+    run(user_name, user_query, prompt, callback)
