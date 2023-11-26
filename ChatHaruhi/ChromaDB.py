2604bbb95b599656b724e084eec66c3f62c090ca
E-sion <101786678+E-sion@users.noreply.github.com>
2023-11-26 12:52:45 +0800

    😶 适配sqlite3旧版本

--- ChatHaruhi/ChromaDB.py
+++ ChatHaruhi/ChromaDB.py
@@ -1,26 +1,21 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 __import__('pysqlite3')
 import sys
 
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-=======
->>>>>>> parent of b7e36d4 (👀 适配老sqlite3版本的linux)
-=======
->>>>>>> parent of b7e36d4 (👀 适配老sqlite3版本的linux)
 import chromadb
 from .BaseDB import BaseDB
 import random
 import string
 import os
 
+
 class ChromaDB(BaseDB):
-    
+
     def __init__(self):
         self.client = None
         self.collection = None
         self.path = None
-    
+
     def init_db(self):
 
         if self.client is not None:
@@ -31,10 +26,10 @@ class ChromaDB(BaseDB):
 
         while os.path.exists(folder_name) or folder_name == '':
             # try to create a folder named temp_<random string> which is not yet existed
-            folder_name =  "tempdb_" + ''.join(random.sample(string.ascii_letters + string.digits, 8))
+            folder_name = "tempdb_" + ''.join(random.sample(string.ascii_letters + string.digits, 8))
 
         self.path = folder_name
-        self.client = chromadb.PersistentClient(path = folder_name)
+        self.client = chromadb.PersistentClient(path=folder_name)
 
         self.collection = self.client.get_or_create_collection("search")
 
@@ -44,15 +39,14 @@ class ChromaDB(BaseDB):
             os.system("cp -r " + self.path + " " + file_path)
             previous_path = self.path
             self.path = file_path
-            self.client = chromadb.PersistentClient(path = file_path)
+            self.client = chromadb.PersistentClient(path=file_path)
             # remove previous path if it start with tempdb
             if previous_path.startswith("tempdb"):
                 os.system("rm -rf " + previous_path)
-                        
 
     def load(self, file_path):
         self.path = file_path
-        self.client = chromadb.PersistentClient(path = file_path)
+        self.client = chromadb.PersistentClient(path=file_path)
         self.collection = self.client.get_collection("search")
 
     def search(self, vector, n_results):
@@ -62,10 +56,9 @@ class ChromaDB(BaseDB):
     def init_from_docs(self, vectors, documents):
         if self.client is None:
             self.init_db()
-        
+
         ids = []
         for i, doc in enumerate(documents):
             first_four_chat = doc[:min(4, len(doc))]
-            ids.append( str(i) + "_" + doc)
-        self.collection.add(embeddings=vectors, documents=documents, ids = ids)
-        
+            ids.append(str(i) + "_" + doc)
+        self.collection.add(embeddings=vectors, documents=documents, ids=ids)
