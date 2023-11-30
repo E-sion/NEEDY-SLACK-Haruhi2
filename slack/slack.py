387b7ea724e775b5bee75aadfc3fa59ca6cee251
E-sion <101786678+E-sion@users.noreply.github.com>
2023-11-30 19:16:52 +0800

    🍳 更换指令调用方式

--- slack/slack.py
+++ slack/slack.py
@@ -19,25 +19,56 @@ prompt, character_db = choose_character(character)
 
 # 处理收到的 DM 消息
 @app.event("message")
-def handle_message_events(event, ack):
-    if is_dm(event):
-        slack_respond_with_agent(ack=ack, app=app, event=event, prompt=prompt, character_db=character_db)
-    return
+def handle_message_events(event, ack, say):
+    ack()
+    if event["text"] == "reset":
+        history_name = character_db.split('/')[-1]
+        with open(f'data/chat_history/{history_name}_history.pkl', 'wb') as f:
+            pickle.dump([], f)
+            say("`已重置对话`")
+            print('已重置对话')
+
+        return
+    else:
+        if is_dm(event):
+            slack_respond_with_agent(ack=ack, app=app, event=event, prompt=prompt, character_db=character_db)
+        return
 
 
 # 群聊
 @app.event("app_mention")
-def handle_mention(event, ack):
-    slack_respond_with_agent(ack=ack, app=app, event=event, prompt=prompt, character_db=character_db, is_thread=True)
+def handle_mention(event, ack, say):
+    ack()
+    if event["text"] == "reset":
+        history_name = character_db.split('/')[-1]
+        with open(f'data/chat_history/{history_name}_history.pkl', 'wb') as f:
+            pickle.dump([], f)
+            say("`已重置对话`")
+            print('已重置对话')
+
+        return
+    else:
+        slack_respond_with_agent(ack=ack, app=app, event=event, prompt=prompt, character_db=character_db,
+                                 is_thread=True)
 
 
-#  重置当前记忆
+#  重置当前记忆 , 此方式重置会出现：slack dispatch_failed ，似乎是一个通用的报错。
 @app.command("/reset")
 def clear_chat_history(say, ack):
+    ack()
     history_name = character_db.split('/')[-1]
     with open(f'data/chat_history/{history_name}_history.pkl', 'wb') as f:
         pickle.dump([], f)
+    say("`已重置对话`")
+    print('已重置对话')
+
+
+@app.command("/forget")
+def clear_chat_history(say, ack):
     ack()
+    history_name = character_db.split('/')[-1]
+    with open(f'data/chat_history/{history_name}_history.pkl', 'wb') as f:
+        pickle.dump([], f)
     say("`已重置对话`")
     print('已重置对话')
 
